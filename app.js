fb94f0b5391e601116339f9b037ed3773ed65721
kenechvkwv <ubesiekene@gmail.com>
2022-12-01 09:24:38 +0100

    set dom from api

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -17,6 +22,12 @@ window.addEventListener("load", () => {
         })
         .then((data) => {
           console.log(data);
+          const { temperature, summary } = data.currently;
+
+          //set DOM Elements from the API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
         });
     });
   }
