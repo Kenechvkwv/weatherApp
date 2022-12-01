81cf72e5dda2651f22bca156164ab13003764b35
kenechvkwv <ubesiekene@gmail.com>
2022-12-01 11:17:01 +0100

    completed celsius to fahrenheit conversion

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ window.addEventListener("load", () => {
   );
   let temperatureDegree = document.querySelector(".temperature-degree");
   let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".degree-section");
+  let temperatureSpan = document.querySelector(".degree-section span");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -21,14 +23,37 @@ window.addEventListener("load", () => {
           return res.json();
         })
         .then((data) => {
-          console.log(data);
-          const { temperature, summary } = data.currently;
+          const { temperature, summary, icon } = data.currently;
 
           //set DOM Elements from the API
           temperatureDegree.textContent = temperature;
           temperatureDescription.textContent = summary;
           locationTimezone.textContent = data.timezone;
+
+          //Formula to convert fahrenheit ot celsius
+          let celsius = (temperature - 32) * (5 / 9);
+
+          //set Icons
+          setIcons(icon, document.querySelector(".icon"));
+
+          //Change temperature to Celsius
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
         });
     });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
